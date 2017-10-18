60b8d6be93473a448d7be7fc04d6d66213ceb315
Travis Carlson <travis.carlson@cs.umn.edu>
2017-10-18 17:48:02 -0500

    update tests

--- test/artifact/Artifact.sv
+++ test/artifact/Artifact.sv
@@ -10,10 +10,9 @@ grammar artifact;
 import edu:umn:cs:melt:ableC:concretesyntax as cst;
 import edu:umn:cs:melt:ableC:drivers:compile;
 
-
 parser extendedParser :: cst:Root {
   edu:umn:cs:melt:ableC:concretesyntax;
-  edu:umn:cs:melt:exts:ableC:skeleton;
+  edu:umn:cs:melt:exts:ableC:watch;
 } 
 
 function main
